a357e1a9ab0c3d5c8446b582574269f72e76736e
Elton Okawa <elton.okawa@gmail.com>
2023-05-13 15:11:21 -0300

    chore: add relay

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RelayEnvironmentProvider } from "react-relay";
+import { RelayEnvironment } from "./RelayEnvironment";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RelayEnvironmentProvider environment={RelayEnvironment}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RelayEnvironmentProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- app/src/react-app-env.d.ts
+++ app/src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+
+
+declare module "babel-plugin-relay/macro" {
+  export { graphql as default } from "react-relay";
+}
