1981f44bad6d68c69e194d210a3be49fd37d879c
Elton Okawa <elton.okawa@gmail.com>
2023-05-12 21:41:33 -0300

    chore: add eslint and linter

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,29 +9,31 @@ const schema = buildSchema(`
   type Query {
     hello: String
   }
-`)
+`);
 
 const root = {
   hello: () => {
-    return 'Hello world!'
+    return 'Hello world!';
   },
-}
+};
 
-const app = express()
+const app = express();
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true,
   })
-)
+);
 
 // In order to avoid exposing our apis, we could check current environment and add or not. e.g.
 // if (NODE_ENV === 'development')
 import expressPlayground from 'graphql-playground-middleware-express';
-app.get('/playground', expressPlayground({ endpoint: GRAPH_QL_ENDPOINT }))
+app.get('/playground', expressPlayground({ endpoint: GRAPH_QL_ENDPOINT }));
 
 app.listen(PORT);
 
-console.log(`Running a GraphQL API server at port '${PORT}' at ${GRAPH_QL_ENDPOINT}`);
+console.log(
+  `Running a GraphQL API server at port '${PORT}' at ${GRAPH_QL_ENDPOINT}`
+);
