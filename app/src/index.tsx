caa2db8b5ecea4a7ebf7f72feccb2a288786465f
Elton Okawa <elton.okawa@gmail.com>
2023-05-21 17:37:45 -0300

    feat: add login page

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     errorElement: <NotFound />,
     children: [
       {
-        path: '/todo',
+        path: '/',
         element: <Todo />,
       },
       {
--- app/src/pages/login/Login.tsx
+++ app/src/pages/login/Login.tsx
@@ -1,3 +1,31 @@
+import { Col, Row, theme } from 'antd';
+import { LoginForm } from './LoginForm';
+
 export function Login() {
-  return <div>login</div>;
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        padding: 24,
+        minHeight: 380,
+        height: '100%',
+      }}
+    >
+      <Col
+        span={8}
+        style={{
+          background: colorBgContainer,
+          padding: '20px',
+          borderRadius: '10px',
+        }}
+      >
+        <LoginForm />
+      </Col>
+    </Row>
+  );
 }
